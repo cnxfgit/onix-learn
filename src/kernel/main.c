df8c14f7eb0e7381b8bf7e5e0eb47994bfa2454f
hlx <1198816371@qq.com>
2024-07-09 15:21:57 +0000

    控制台

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,44 +1,23 @@
+#include <onix/console.h>
 #include <onix/io.h>
 #include <onix/onix.h>
+#include <onix/stdarg.h>
 #include <onix/string.h>
 #include <onix/types.h>
 
-// #define CRT_ADDR_REG 0x3d4
-// #define CRT_DATA_REG 0x3d5
+void test_args(int cnt, ...) {
+    va_list args;
+    va_start(args, cnt);
 
-// #define CRT_CURSOR_H 0xe
-// #define CRT_CURSOR_L 0xf
-
-char message[] = "hello onix!!!";
-char buf[1024];
+    int arg;
+    while (cnt--) {
+        arg = va_arg(args, int);
+    }
+    va_end(args);
+}
 
 void kernel_init() {
-    // outb(CRT_ADDR_REG, CRT_CURSOR_H);
-    // u16 pos = inb(CRT_DATA_REG) << 8;
-    // outb(CRT_ADDR_REG, CRT_CURSOR_L);
-    // pos |= inb(CRT_DATA_REG);
-
-    // outb(CRT_ADDR_REG, CRT_CURSOR_H);
-    // outb(CRT_DATA_REG, 0);
-    // outb(CRT_ADDR_REG, CRT_CURSOR_L);
-    // outb(CRT_DATA_REG, 0);
-
-    int res;
-    res = strcmp(buf, message);
-    strcpy(buf, message);
-    res = strcmp(buf, message);
-    strcat(buf, message);
-    res = strcmp(buf, message);
-    res = strlen(message);
-    res = sizeof(message);
- 
-    char *ptr = strchr(message, '!');
-    ptr = strrchr(message, '!');
-
-    memset(buf, 0, sizeof(buf));
-    res = memcmp(buf, message, sizeof(message));
-    memcpy(buf, message, sizeof(message));
-    res = memcmp(buf, message, sizeof(message));
-    ptr = memchr(buf, '!',  sizeof(message));
+    console_init();
+    test_args(5, 1, 0xaa, 5, 0x55, 10);
     return;
 }
